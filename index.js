2928e68b54a1fcca700de22df48ed08ce613640d
Noel Fernandez <noelfernandez98@gmail.com>
2021-11-03 14:54:42 +0000

    reverse Frank complete algorithm

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 //write your code here
+function reverseFunction (str) {
+    let reverse = "";
+    for(let i = str.length-1; i >= 0; i--){
+        reverse += str[i]
+    } 
+    return reverse
+}
+console.log(reverseFunction("Frank"))
